01d20a515e325f1724674cb469f8be18d9abf011
NicolasSmere <nicolassmereok@gmail.com>
2022-11-09 15:54:09 -0300

    js agregado

--- Script.js
+++ Script.js
@@ -1,2 +1,8 @@
 var prueba = document.getElementById("Prueba3");
 prueba.innerHTML = "PruebaNueva";
+
+var theButton = document.getElementById("chau");
+
+function desaparece (){
+    theButton.style.display = "none" ;
+}
